a621c6b55d9c02fe497b3854dde75c9d558cb033
TaimoorZahid86 <taimoorbinzahid86@gmail.com>
2023-02-03 12:53:07 +0500

    Created layout.html for template inheritance.

--- flaskblog.py
+++ flaskblog.py
@@ -2,14 +2,28 @@ from flask import Flask
 from flask import render_template
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Taimoor Bin Zahid',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Umair Khalid',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
 
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='About')
 
 if __name__ == "__main__":
     app.run(debug=True)
