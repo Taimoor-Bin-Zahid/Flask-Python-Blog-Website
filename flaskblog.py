1dff9d0c6611ba954d05486fa8015aad8eadf2f9
TaimoorZahid86 <taimoorbinzahid86@gmail.com>
2023-02-21 12:01:51 +0500

    /

--- flaskblog.py
+++ flaskblog.py
@@ -15,11 +15,15 @@ db = SQLAlchemy(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username =db.column(db.String(20), nullable=False, unique=True)
-    email = db.column(db.String(120), nullable=False, unique=True)
-    image_file = db.column(db.String(120), nullable=False, unique=False, default='default.jpg')
-    password = db.Column(db.string(60), nullable=False)
+    username =db.Column(db.String(20), nullable=False, unique=True)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    image_file = db.Column(db.String(120), nullable=False, unique=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
 
+
+
+def __repr__(self):
+    return f"User('{self.username}, {self.email}, {self.image_file}')"
 
 
 
