38a18cdfd114649364a3b18d125f0ea2c3beba87
TaimoorZahid86 <taimoorbinzahid86@gmail.com>
2023-02-21 15:15:13 +0500

    .

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,15 +1,7 @@
-from flask import Flask, render_template, url_for, flash, redirect
-from flask_sqlalchemy import SQLAlchemy
-from forms import RegistrationForm, LoginForm
-
-
-
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
-from models import User, Post
+from flask import render_template, url_for, flash, redirect
+from flaskblog.models import User, Post
+from flaskblog.forms import RegistrationForm, LoginForm
+from flaskblog import app
 
 
 
@@ -64,8 +56,3 @@ def login():
         else:
             flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
-
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
